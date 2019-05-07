5a444b29c28234fbafe77d7de3dac86e3fd20dcf
Doron Behar <doron.behar@gmail.com>
2019-05-07 18:51:35 +0300

    Enable using a different instance for embeds and normal redirects

--- background.js
+++ background.js
@@ -1,27 +1,30 @@
-function createBlockingRequest(url, domain) {
+function createBlockingRequest(url, domain, embed_domain) {
   if (url.hostname.endsWith("youtube.com") || url.hostname.endsWith("youtube-nocookie.com")) {
     console.log('youtube detected in url ' + url)
     const v = url.searchParams.get("v");
     const q = url.searchParams.get("q") || url.searchParams.get("search_query");
     if (v) {
-      return {redirectUrl: `https://` + domain + `/watch?v=${v}`}
+      return {redirectUrl: domain + `/watch?v=${v}`}
     } else if (q) {
-      return {redirectUrl: `https://` + domain + `/search?q=${q}`}
+      return {redirectUrl: domain + `/search?q=${q}`}
     } else if (url.pathname.startsWith("/channel/")) {
-      return {redirectUrl: `https://` + domain + `${url.pathname}`}
+      return {redirectUrl: domain + `${url.pathname}`}
     } else if (url.pathname.startsWith("/embed/")) {
-      return {redirectUrl: `https://` + domain + `${url.pathname}`}
+      return {redirectUrl: embed_domain + `${url.pathname}`}
     }
   } else if (url.hostname.endsWith("youtu.be")) {
-    return {redirectUrl: `https://` + domain + `/watch?v=${url.pathname.replace("/", "")}`}
+    return {redirectUrl: domain + `/watch?v=${url.pathname.replace("/", "")}`}
   }
 }
 
 browser.webRequest.onBeforeRequest.addListener(request => {
   const url = new URL(request.url);
   return browser.storage.sync.get('instance').then((res) => {
-    var instance = res.instance || 'invidio.us';
-    return createBlockingRequest(url, instance)
+    var instance = res.instance || 'https://invidio.us';
+    return browser.storage.sync.get('embed_instance').then((res) => {
+      var embed_instance = res.embed_instance || 'https://invidio.us';
+      return createBlockingRequest(url, instance, embed_instance)
+    })
   });
 },
   {
--- options.js
+++ options.js
@@ -1,22 +1,32 @@
+const options_selectors = ['instance', 'embed_instance']
+
 function saveOptions(e) {
-  var inst = document.querySelector("#instance").value;
-  var real_instance;
-  try {
-    real_instance = new URL(inst).hostname;
-  } catch (e) {
-    real_instance = inst;
-  }
-  browser.storage.sync.set({
-    instance: real_instance
-  });
-  e.preventDefault();
+  options_selectors.forEach(sel => {
+    var inst = document.querySelector("#" + sel).value;
+    var real_instance;
+    try {
+      real_instance = new URL(inst).origin;
+    } catch (e) {
+      try {
+        real_instance = new URL('https://' + inst).origin;
+      } catch (e) {
+        real_instance = 'https://invidio.us'
+      }
+    }
+    browser.storage.sync.set({
+      [sel]: real_instance
+    });
+    e.preventDefault();
+  })
 }
 
 function restoreOptions() {
-  var gettingItem = browser.storage.sync.get('instance');
-  gettingItem.then((res) => {
-    document.querySelector("#instance").value = res.instance || 'invidio.us';
-  });
+  options_selectors.forEach(sel => {
+    var gettingItem = browser.storage.sync.get(sel);
+    gettingItem.then((res) => {
+      document.querySelector("#" + sel).value = res[sel] || 'https://invidio.us';
+    });
+  })
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
